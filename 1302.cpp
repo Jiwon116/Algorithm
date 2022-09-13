c24448232ed09e6dffd897d9384eceba14a5a07c
jiwon <ji_1106@naver.com>
2022-09-13 23:55:04 +0900

    BOJ

---
+++ 1302.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+
+    int n; cin >> n;
+
+    map<string, int> m;
+
+    for(int i = 0; i < n; ++i) {
+        string name; cin >> name;
+        
+        if (m.find(name) != m.end()) m[name] += 1;
+        else m[name] = 1;
+    }
+
+    string answer = "";
+    int max_count = 0;
+    for(auto e = m.begin(); e != m.end(); ++e) {
+        if (max_count < (e -> second)) {
+            max_count = (e -> second);
+            answer = (e -> first);
+        } else if (max_count = (e -> second)) {
+            if ((e -> first) < answer) answer = (e -> first);
+        }
+    }
+
+    cout << answer << '\n';
+
+    return 0;
+}
