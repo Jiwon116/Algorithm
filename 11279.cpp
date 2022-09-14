51d4d808a8b18896f53a4cd20cc6a68cf0cf650b
jiwon <ji_1106@naver.com>
2022-09-14 23:27:38 +0900

    BOJ

---
+++ 11279.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    cin.sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+
+    priority_queue<int> pq;
+    while(n--) {
+        int num; cin >> num;
+
+        if(num == 0) {
+            if(pq.empty()) cout << 0 << '\n';
+            else {
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        } else {
+            pq.push(num);
+        }
+    }
+
+    return 0;
+}
