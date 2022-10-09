6a5be4830edf4bb2c2e5b8508ef838ba4fa99397
jiwon <ji_1106@naver.com>
2022-10-09 23:53:37 +0900

    BOJ

---
+++ 1018.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+char board[51][51];
+
+int main() {
+    int n, m; cin >> n >> m;
+
+    for (int x = 0; x <= n - 8; ++x) {
+        for (int y = 0; y <= m - 8; ++y) {
+
+        }
+    }
+
+
+    return 0;
+}
