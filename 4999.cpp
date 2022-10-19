b850655e4ae800ac3a3fd62bd0ee5685df80f19d
jiwon <ji_1106@naver.com>
2022-10-19 23:31:59 +0900

    BOJ

---
+++ 4999.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s1, s2; cin >> s1 >> s2;
+
+    if (s1.length() >= s2.length()) cout << "go" << '\n';
+    else cout << "no" << '\n';
+
+    return 0;
+}
