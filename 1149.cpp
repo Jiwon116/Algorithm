864d73112ce33bb4d75aaabe64725b1449e024d9
jiwon <ji_1106@naver.com>
2022-10-31 01:05:40 +0900

    BOJ

--- 1149.cpp
+++ 1149.cpp
@@ -4,23 +4,29 @@
 
 #define RED 0       // 빨강
 #define GREEN 1     // 초록
-#define BULE 2      // 파랑
+#define BLUE 2      // 파랑
 
 using namespace std;
 
 int n; 
-int board[1001][1001];
-ll memo[1001][1001][3];
+int board[1001][1001]; // 색칠하는 비용
+ll memo[1001][3];
 
-int dp(int x, int y, int color) {
+ll dp(int x, int color) {
     // ret이 나타내는 건 최소 비용
     // 따라서 ret이 가장 작은지 확인한 후 return 해줘야 함
 
-    if (x == n - 1 && y == 3 ) return 0;
+    if (x == n) return 0;
 
-    ll &ret = memo[x][y][color];
+    ll &ret = memo[x][color];
     if(ret != -1) return ret;
 
+    ret = 1e9;
+
+    for(int i = 0; i < 3; ++i) {
+        if (i == color) continue;
+        ret = min(ret, dp(x + 1, i) + board[x][i]);
+    }
 
     return ret;
 }
@@ -29,8 +35,6 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
-    memset(memo, -1, sizeof(memo));
     
     cin >> n;
 
@@ -40,7 +44,16 @@ int main() {
         }
     }
 
-    dp(0, 0, 0);
+    ll answer = 1e9;
+    // dp(0, 0) 부터 시작
+    for(int i = 0; i < 3; ++i) {
+        memset(memo, -1, sizeof(memo));
+        // dp(x, color)는 집 x를 칠할건데, 이전의 집(x-1)이 i 색깔이다
+        // 따라서, x는 i 색깔이면 안된다
+        answer = min(answer, dp(1, i) + board[0][i]);
+    }
+
+    cout << answer << '\n';
 
     return 0;
 }
