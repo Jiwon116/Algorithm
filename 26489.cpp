b93f521ffd8eac6903b1dbef39a5e5154c7efa4e
jiwon <ji_1106@naver.com>
2023-02-20 22:05:31 +0900

    BOJ

---
+++ 26489.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    string s;
+    int num = 0;
+
+    while(getline(cin, s)) {num++;}
+
+    cout << num << '\n';
+    
+    return 0;
+}
