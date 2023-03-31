410bfc3e29a0ab301af34326d7e1110106a2d167
Jiwon116 <ji_1106@naver.com>
2023-03-31 23:58:11 +0900

    BOJ

---
+++ 10189.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    cout << "#  # #### #### #  #" << '\n';
+    cout << "#### #  # #  # # #" << '\n';
+    cout << "#### #  # #  # # #" << '\n';
+    cout << "#  # #### #### #  #" << '\n';
+    
+    return 0;
+}
