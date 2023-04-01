0885ce3225ef922b70d5aec66f27019e7800961c
Jiwon116 <ji_1106@naver.com>
2023-04-01 23:58:31 +0900

    BOJ

---
+++ 24264.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    ll N; cin >> N;
+	cout << N * N << '\n' << 2;
+    
+    return 0;
+}
