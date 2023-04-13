72b07811358c19f65d8067598d47d55db16e6ec3
Jiwon116 <ji_1106@naver.com>
2023-04-13 15:34:47 +0900

    Programmers

---
+++ Programmers/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    answer += toupper(s[0]);
+    
+    for(int i = 1; i < s.size(); ++i) {
+        if(s[i - 1] == ' ') answer += toupper(s[i]);
+        else answer += tolower(s[i]);
+    }
+    
+    return answer;
+}
